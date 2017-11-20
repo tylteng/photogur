b4fc6f8cc17141bc3a4866abdc655dc0540142f6
tylteng <teng.ty4@gmail.com>
2017-11-20 15:40:23 -0500

    defined create method (saved new picture to database)

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,11 +8,22 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
   end
 
-  def create
-  end
-
   def new
     @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      redirect_to "/pictures"
+    else
+      render :new
+    end
   end
 
 end
